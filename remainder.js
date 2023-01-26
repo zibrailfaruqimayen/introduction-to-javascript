fc5ee07bd68a8f77282b9a4253e7d2550c459afa
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-26 22:27:04 +0600

    vagfol

---
+++ remainder.js
@@ -0,0 +1,7 @@
+var mangoes = 19;
+var parson = 7;
+var remainder = mangoes % parson;
+
+//  /this means vagfol
+// % this means vagses
+console.log(remainder);
---
+++ types.js
@@ -0,0 +1,15 @@
+var price = 100;
+// console.log(typeof price);
+var price = '100';
+// console.log(typeof price);
+var isHappy = true;
+// console.log(isHappy);
+
+
+//advanced
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+sum = parseFloat(sum);
+console.log(sum);
