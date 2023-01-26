9a39d55139af97d158e6485ee868b970b33a2ff4
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-26 22:47:36 +0600

    vagfol

--- remainder.js
+++ remainder.js
@@ -4,4 +4,8 @@ var remainder = mangoes % parson;
 
 //  /this means vagfol
 // % this means vagses
-console.log(remainder);
+// console.log(remainder);
+
+var mangoes = 17;
+var parson = 5;
+console.log(17%5)
