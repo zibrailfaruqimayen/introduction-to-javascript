45a08ee9af836fc7d9baa87b2b131cfa94258ed3
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-26 16:11:06 +0600

    js variable

--- math.js
+++ math.js
@@ -1,3 +1,8 @@
 /***
  * 1. number===> 55, 898, -32
+ * 2. integer: 2, 5,
+ * 3. float: decimal: 2.5, 13.67, 32.69
+ * 4. operation: addition, subtraction, multiplication, division, remainder
+ * 5. percent
+ * 6. average
  * */
--- variable.js
+++ variable.js
@@ -1,2 +1,7 @@
 //vary + able = variable
 var price = 13; 
+var age = 17;
+var temperature = 38;
+var weight = 80;
+var money = 5000;
+var cost = 10000;
