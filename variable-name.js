169b2e97028febdf6a95367a422a752b79a3283a
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-26 17:08:09 +0600

    js type of variable name

---
+++ variable-name.js
@@ -0,0 +1,21 @@
+// good variable
+var price = 29;
+
+// 1. variable name can not be any keyword
+var false = 96;
+var return = true; 
+// 2. variable name has to be in one word. no space
+var my home address = "new california";
+
+// 3. variable name can not have quotation
+var "name" = "tom hanks";
+
+
+// how to use long names 
+// can not use dash 
+
+var user_name = "bappa raj";
+var usercurrenthomeaddress = "andor killa bandoe ban";
+var user_home_address = "andor killa bandoe ban"; //snake case
+var userHomeAddress = "andor killa bandoe ban"; // camel case: we will use this one
+var UserHomeAddress = "andor killa bandoe ban"; // pascal case
