e596c879980306537ae137c843df1f82cbe1492e
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-26 19:02:25 +0600

    operetion

---
+++ operation.js
@@ -0,0 +1,29 @@
+var onionprice = 20;
+var eggprice = 10;
+
+
+// addition, subtraction 
+var totalPrice = onionprice + eggprice;
+var pricedifference = onionprice - eggprice;
+
+// console.log(pricedifference);
+
+
+// console.log(totalPrice);
+// console.log(onionprice);
+// console.log(eggprice);
+// console.log(onionprice + eggprice);
+
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+
+// division 
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
--- variable.js
+++ variable.js
@@ -1,4 +1,6 @@
 //vary + able = variable
+
+//numeric
 var price = 13; 
 var age = 17;
 var temperature = 38;
@@ -6,6 +8,13 @@ var weight = 80;
 var money = 5000;
 var cost = 10000;
 
+
+// string
 var person = "sodor uddin";
 var location = "andor killa bandorban";
-var special = 'alia bhatt' ;
+var special = 'alia bhatt';
+
+//boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
